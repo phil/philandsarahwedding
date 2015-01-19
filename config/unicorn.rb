7c6bfc1c650b106c0f4ef3743ef947fd03ffb36b
Phil Balchin <phil@maniacalrobot.co.uk>
2015-01-19 13:57:04 +0000

    Initial Commit

---
+++ config/unicorn.rb
@@ -0,0 +1,17 @@
+worker_processes 4
+preload_app true
+timeout 300
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+end
