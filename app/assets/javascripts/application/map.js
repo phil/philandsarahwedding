bfb09b855ea4fde6ff13d3c1b497b91a64ed9e4a
Phil Balchin <phil@maniacalrobot.co.uk>
2015-01-26 21:54:12 +0000

    style update

--- app/assets/javascripts/application/map.js
+++ app/assets/javascripts/application/map.js
@@ -7,20 +7,16 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  //var palaceWindow = new google.maps.InfoWindow({
-  //content: mapWindowContent("Mirabell Palace")
-  //});
-
   var markers = [
-  { name : "Hotel &amp; Villa Auersperg", lat : 47.806844, lng : 13.049722 },
-  { name : "Mirabell Palace", lat : 47.8054699, lng : 13.0416727 },
-  { name : "Café Classic", lat : 47.802677, lng : 13.0438077 },
-  { name : "K+K Restuarant", lat : 47.799367, lng : 13.047225 }
+    { name : "Hotel&nbsp;&amp;&nbsp;Villa&nbspAuersperg", lat : 47.806844, lng : 13.049722 },
+    { name : "Mirabell&nbsp;Palace", lat : 47.8054699, lng : 13.0416727 },
+    { name : "Café&nbsp;Classic", lat : 47.802677, lng : 13.0438077 },
+    { name : "K+K&nbsp;Restuarant", lat : 47.799367, lng : 13.047225 }
   ]
 
-    for (i = 0; i < markers.length; i++) {
-      addMarkerToMap(markers[i], map);
-    }
+  for (i = 0; i < markers.length; i++) {
+    addMarkerToMap(markers[i], map);
+  }
 
   function addMarkerToMap(marker, map) {
     var mapWindow = new google.maps.InfoWindow({
@@ -37,29 +33,6 @@ function initialize() {
       mapWindow.open(map,mapMarker);
     });
   }
-
-
-  //var palaceMarker = new google.maps.Marker({
-  //position: new google.maps.LatLng(47.8054699,13.0416727),
-  //map: map,
-  //title: "Mirabell Palace"
-  //});
-
-  //google.maps.event.addListener(palaceMarker, 'click', function() {
-  //palaceWindow.open(map,palaceMarker);
-  //});
-
-  //var kkMarker = new google.maps.Marker({
-  //position: new google.maps.LatLng(47.799367,13.047225),
-  //map: map,
-  //title: "K+K Restuarant"
-  //});
-
-  //var kkHotel = new google.maps.Marker({
-  //position: new google.maps.LatLng(47.806844,13.049722),
-  //map: map,
-  //title: "Hotel + Villa Auersperg"
-  //});
 
   function mapWindowContent(title) {
     return "<div class='map-window'>" + title + "</div>";
